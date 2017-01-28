749d116b3f69bb9afe1197b46745547c55e65858
Romulo Soares <bromulosoares@gmail.com>
2017-01-28 20:39:32 -0300

    Armazenado . . .

--- Resolução 1ª Lista de Exercícios - P1_ADS_ALP/Questao2.pas
+++ Resolução 1ª Lista de Exercícios - P1_ADS_ALP/Questao2.pas
@@ -1,12 +1,12 @@
 Program Pzim ;
 uses crt;
 var
-	ano,aux:integer;
+	ano,aux, mes:integer;
 	bissexto:boolean;
 
 Begin
 	write('Escreva o mês que deseja saber a quantidade de dias: ');
-	readln(ano);
+	readln(mes);
 	write('Escreva o ano: ');
 	readln(ano);
 	if (ano mod 4 =0) then bissexto:=true
@@ -20,7 +20,20 @@ Begin
 		end;
 	end
 	else bissexto:=false;
-	writeln('O seu ano é: ',bissexto);
+	if((mes=1) or (mes=3) or (mes=5) or (mes=7) or (mes=8) or (mes=10) or (mes=12)) then
+		writeln('O mês tem 31 dias.')
+	else
+	begin
+		if((bissexto) and (mes=2)) then
+			writeln('O mês tem 29 dias.')
+		else if (mes=2) then
+			writeln('O mês tem 28 dias.')
+		else writeln('O mês tem 30 dias.');
+	end;
+
+			
+		
+	
 	
 
 readln;  
--- Resolução 1ª Lista de Exercícios - P1_ADS_ALP/Questao3.pas
+++ Resolução 1ª Lista de Exercícios - P1_ADS_ALP/Questao3.pas
@@ -1,24 +1,44 @@
 Program Pzim ;
 uses crt;
-var
 
+const
+	horasMaxDia = 24;
+	minutosMaxHora = 60;
+	fusoMaxHorario = 24;
+var
+	horas, minutos,fuso, horaNoFuso: integer;
+	
 Begin
 	clrscr;
 	write('Escreva a quantidade de horas: ');
 	read(horas);
-	write('Escreva a quantidade de minutos: ');
-	read(minutos);
-	write('Escreva o fuso: ');
-	read(fuso);
-	tempo:= (horas/60) + minutos;
-	if(fuso>0) then
-		if(tempo+fuso>24) then
-			horaComFuso:= ((tempo + fuso)-(24*60))/60
-		else horaComFuso:= (tempo+fuso)/60;
-	else if(fuso<0) and ((fuso*-1)<hora)) then
-		horaComFuso:=  
-	
-	
-	
+	if (horas>horasMaxDia-1) then 
+		writeln('Erro! Um dia tem 24 horas, mas o maximo que pode ser representado são 23.')
+	else
+	begin
+		write('Escreva a quantidade de minutos: ');
+		read(minutos);
+		if(minutos>minutosMaxHora-1) then
+			writeln('Erro! Uma hora tem 60 minutos, mas o maximo comum usado é 59.')
+		else
+		begin
+			write('Escreva o fuso: ');
+			read(fuso);
+			if (fuso>fusoMaxHorario) then
+				writeln('Erro! Existem apenas 24 fusos horários.')
+			else
+			begin
+				horaNoFuso:= horas+(fuso);
+				if((fuso<0) and ((fuso*-1)>horas)) then
+				begin
+					if(horas=0) then horas:=24;
+					horaNoFuso:=(24+horas)+fuso;
+				end;			
+				if(horaNoFuso>24) then horaNoFuso:=horaNoFuso-24;
+				if(horaNoFuso=24) then horaNoFuso:=0;
+				writeln('O horário no fuso informado é: ',horaNoFuso,':',minutos,' hrs.');
+		  end;
+		end;	
+	end;
 	readln;  
 End.
